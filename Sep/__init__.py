8c9eab9e58ac6ba0f6f3edfdeb5d65baeafa09cd
Abderafie <chairi.abderafie@srtudent.emi.ac.ma>
2022-08-22 21:25:20 +0200

    first commit

--- Sep/__init__.py
+++ Sep/__init__.py
@@ -7,7 +7,6 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_cors import CORS
 from .create_db import create
-from flask_migrate import Migrate
 
 def create_app(config):
 	app=Flask(__name__)
@@ -28,7 +27,6 @@ def create_app(config):
 	mail.init_app(app)
 	Bootstrap(app)
 	CORS(app)
-	Migrate(app,db)
 	# sysAdmin.init_app(app)
 
 	return app
